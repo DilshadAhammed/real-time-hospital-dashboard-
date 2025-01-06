3f51f7c35220f758489c49dcb2b6bfa1dbd80753
DilshadAhammed <dilshadnakunnath@gmail.com>
2025-01-06 07:11:03 +0530

    initial

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,25 +1,55 @@
 import React from "react";
-import "./styles.css";
+// import "./styles.css";
+import landing from "../assets/landing.png";
 
 function Landing() {
   return (
     <>
-      <header>
+      <header className="w-full absolute top-5 text-center">
         <nav>
-          <a href="#">Home</a>
-          <a href="#page-2">About</a>
-          <a href="#">Contact</a>
+          <a href="#" className=" mr-10">
+            Home
+          </a>
+          <a href="#page-2" className=" mr-10">
+            About
+          </a>
+          <a href="#page-2" className=" mr-10">
+            Doctor
+          </a>
+          <a href="#" className=" mr-10">
+            Contact
+          </a>
         </nav>
       </header>
-      <main>
-        <h1>Explore the world.</h1>
+      <main className="bg-[#F9F3F3] h-screen flex justify-center items-center">
+        <div className="container mx-auto bg-white py-10 flex justify-around items-center rounded-3xl">
+          <div className="ml-10 ">
+            <h2 className="text-5xl font-serif">Empowering</h2>
+            <h2 className="text-5xl font-serif">lives through</h2>
+            <h2 className="text-5xl font-serif ">Health care</h2>
+            <div className="absolute mt-10">
+              <p className="font-mono w-3/4">
+                How can we assist you today? Please select the following option
+                !
+              </p>
+              <button className="mt-2 bg-[#4239F2BD] text-white font-bold py-2 px-4 rounded-2xl">
+                Show more
+              </button>
+            </div>
+          </div>
+          <div>
+            <img src={landing} alt="" />
+          </div>
+        </div>
       </main>
-      <div className="container" id="page-2">
-        <h1>Explore the universe</h1>
-      </div>
-      {/* <main>
-        
-      </main> */}
+      <main className="bg-[#F9F3F3] h-screen flex justify-center flex-col relative">
+        <h1 className="absolute top-10 text-center w-full text-3xl font-bold ">3 Easy steps and get your medical solutions with us</h1>
+        <div className="container mx-auto bg-white py-10 flex justify-around items-center rounded-3xl">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </main>
     </>
   );
 }
